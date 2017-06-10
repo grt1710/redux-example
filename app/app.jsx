208ad79567147c47797caaa93e8a4f0fc97535d5
Ravi Teja <raviteja@Ravis-MacBook-Pro.local>
2017-06-10 12:24:54 +0530

    redux

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {IndexRoute, Route, Router, hashHistory, HashRouter} = require('react-router');
+
+// Load css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+    <div>
+      <h1>Hello World</h1>
+    </div>,
+    document.getElementById('app')
+);
+
+require('./redux-example.jsx');
